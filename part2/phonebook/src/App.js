8d2e1c283b942f2a2d4ad581b3b9cb349672abe8
Stormgren <sbnv93@gmail.com>
2021-04-01 23:19:59 +0200

    Implemented axios and use effect to phonebook app, exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Search from './components/Search'
 import Form from './components/Form'
@@ -7,10 +8,6 @@ import Contacts from './components/Contacts'
 const App = () => {
   
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
 
   const [ newName, setNewName ] = useState('')
@@ -19,6 +16,13 @@ const App = () => {
   const [searchRes, setSearchRes] = useState('')
 
 
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons')
+    .then(res => {
+      setPersons(res.data)
+    })
+  }, [])
 
   const formHandler = (e) => {
     e.preventDefault();
