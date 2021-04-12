6225dbdb5f0fbb3a640963332037007a446406be
Stormgren <sbnv93@gmail.com>
2021-04-12 12:22:16 +0200

    Refactored countries API code

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,118 +1,145 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
-
-  const [countries, setCountries] = useState([])
-  const [search, setSearch] = useState(false);
-  const [searchRes, setSearchRes] = useState('')
-  const [message, setMessage] = useState('')
-  const [results, setResults] = useState([])
-  const [details, setDetails] = useState({})
-  const [stats, setStats] = useState(false)
-  const [one, setOne] = useState('')
-  const [weather, setWeather] = useState([])
+  const [countries, setCountries] = useState([]);
+  const [searchRes, setSearchRes] = useState("");
+  const [message, setMessage] = useState("");
+  const [results, setResults] = useState([]);
+  const [details, setDetails] = useState({});
+  const [stats, setStats] = useState(false);
+  const [one, setOne] = useState("");
+  const [weather, setWeather] = useState([]);
 
   useEffect(() => {
-    axios.get('https://restcountries.eu/rest/v2/all').then((res) => 
-      setCountries(res.data)
-      )
-    
-  }, [])
-  
+    axios
+      .get("https://restcountries.eu/rest/v2/all")
+      .then((res) => setCountries(res.data));
+  }, []);
+
   const formHandler = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+  };
 
   const searchHandler = (e) => {
-    setSearchRes(e.target.value)
+    setSearchRes(e.target.value);
     let currentquery = null;
 
+    currentquery = countries.filter((country) => {
+      return country.name.toLowerCase().includes(searchRes.toLowerCase());
+    });
 
-    currentquery = countries.filter(country => {
-      return (
-        country.name.toLowerCase().includes(searchRes.toLowerCase()) 
-        );
-      });
-
-      if (currentquery !== '' && currentquery.length > 10){
-        setMessage('The results are too many, please be more specific')
-      } else if(currentquery.length !== 1){
-        setMessage('')
-        setResults(currentquery)
-       console.log(results)
-        
-      } else if(currentquery.length === 1) {
-        setMessage('')
-        setResults(currentquery)
-        let data = results[0];
-          let countryObj = {
+    if (currentquery !== "" && currentquery.length > 10) {
+      setMessage("The results are too many, please be more specific");
+    } else if (currentquery.length !== 1) {
+      setMessage("");
+      setResults(currentquery);
+    } else if (currentquery.length === 1) {
+      setMessage("");
+      setResults(currentquery);
+      
+      let data = results[0];
+     
+      let countryObj = {
         name: data.name,
         capital: data.capital,
         population: data.population,
         languages: data.languages,
-        flag: data.flag
-      }
-      setDetails(countryObj)
-      }
-    
-  }
+        flag: data.flag,
+      };
+      setDetails(countryObj);
+    }
+  };
 
-  
   const showDetailsHandler = (res) => {
-    setStats(!stats)
-    setOne(res)
-  }
+    setStats(!stats);
+    setOne(res);
+  };
 
   useEffect(() => {
-    axios.get(`https://restcountries.eu/rest/v2/name/${one}`).then(str =>  setDetails(str.data[0]))
-  }, [stats])
+    axios
+      .get(`https://restcountries.eu/rest/v2/name/${one}`)
+      .then((str) => setDetails(str.data[0]));
+  }, [one]);
 
   useEffect(() => {
-    if (details != undefined){
-    let info = details.capital;
-    axios.get(`http://api.weatherstack.com/current?access_key=4eb13e5e5f94ea2fe39b16fb057056b4&query=${info}`)
-    .then(res => setWeather(res.data))
-  }}, [details])
+    if (details !== undefined) {
+      let info = details.capital;
+      const KEY = process.env.REACT_APP_API_KEY.replace(/'/g, "");
+      axios
+        .get(
+          `http://api.weatherstack.com/current?access_key=${KEY}&query=${info}`
+        )
+        .then((res) => setWeather(res.data));
+    }
+  }, [details]);
 
   const Data = ({ details, weather }) => {
-    return(
+    return (
       <div>
-      <h3>{details.name}</h3>
-       {details.capital != null ? <p> Capital {details.capital}</p> : <br/>}
-      {details.population != null ? <p>Population: {details.population}</p> : <br/>}
-      <ul>
-        {details.languages !== undefined ? details.languages.map(lang => <li key={lang.name}>{lang.name}</li>) : <br/>}
-      </ul>
-      <img src={details.flag} alt={details.name}/>
-      <p>Weather of {details.name}</p>
-      {weather.current != undefined ? <p>Temperature is {weather.current.temperature}</p> : <br/>}
-      {weather.current != undefined ?<img src={weather.current.weather_icons} alt={weather.current.description}/> : <br/>}
-      {weather.current != undefined ?<p>Wind speed {weather.current.wind_speed} direction {weather.current.wind_dir}</p> : <br/>}
- 
- 
+        <h3>{details.name}</h3>
+        {details.capital != null ? <p> Capital {details.capital}</p> : <br />}
+        {details.population != null ? (
+          <p>Population: {details.population}</p>
+        ) : (
+          <br />
+        )}
+        <ul>
+          {details.languages !== undefined ? (
+            details.languages.map((lang) => (
+              <li key={lang.name}>{lang.name}</li>
+            ))
+          ) : (
+            <br />
+          )}
+        </ul>
+        <img className="flag" src={details.flag} alt={details.name} />
 
-
+        {details.name != null ? <p>Weather of {details.name}</p> : <br />}
+        {weather.current !== undefined ? (
+          <p>Temperature is {weather.current.temperature}</p>
+        ) : (
+          <br />
+        )}
+        {weather.current !== undefined ? (
+          <img
+            src={weather.current.weather_icons}
+            alt={weather.current.description}
+          />
+        ) : (
+          <br />
+        )}
+        {weather.current !== undefined ? (
+          <p>
+            Wind speed {weather.current.wind_speed} direction{" "}
+            {weather.current.wind_dir}
+          </p>
+        ) : (
+          <br />
+        )}
       </div>
-    )
-  }
-
+    );
+  };
 
   return (
     <div className="App">
       <form onSubmit={formHandler}>
-        <input type="text" onChange={searchHandler}/>
-
-
+        <input type="text" onChange={searchHandler} />
       </form>
       {message}
       <ul>
-      {results.map(result => <li key={result.name}>{result.name} <button onClick={() => showDetailsHandler(result.name)}>Show more</button> </li>)}
+        {results.map((result) => (
+          <li key={result.name}>
+            {result.name}
+            <button onClick={() => showDetailsHandler(result.name)}>
+              Show more
+            </button>
+          </li>
+        ))}
       </ul>
-      { <Data details={details} weather={weather}/> }
+      {<Data details={details} weather={weather} />}
     </div>
   );
-}
+};
 
 export default App;
