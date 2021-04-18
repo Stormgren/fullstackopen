323000516710ff74b75798d5a49a14587dfdfa67
Stormgren <sbnv93@gmail.com>
2021-04-18 22:23:04 +0200

    Finished 3.13 part

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,14 +2,13 @@ const { request, response } = require('express');
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-
+require('dotenv').config()
 const app = express();
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(cors());
-// Morgan for usage with tiny configuration
-// app.use(morgan('tiny'))
 
 morgan.token('body', (request, response) => {
     return JSON.stringify(request.body)
@@ -18,104 +17,115 @@ morgan.token('body', (request, response) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Marry Poppendick",
-        number: "39-23-6423122"
-    }
+// let persons = [
+//     {
+//         id: 1,
+//         name: "Arto Hellas",
+//         number: "040-123456"
+//     },
+//     {
+//         id: 2,
+//         name: "Ada Lovelace",
+//         number: "39-44-5323523"
+//     },
+//     {
+//         id: 3,
+//         name: "Dan Abramov",
+//         number: "12-43-234345"
+//     },
+//     {
+//         id: 4,
+//         name: "Marry Poppendick",
+//         number: "39-23-6423122"
+//     }
     
-]
+// ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(data => {
+        response.json(data)
+    })
 })
+
+// app.get('/api/persons/:id', (request, response) => {
+//     const id = String(request.params.id)
+//     const person = persons.find(person => person.id === id)
+
+//     if(person) {
+//         response.json(person)
+//     } else {
+//         response.status(404).end()
+//         console.log(response.status(404).end())
+//     }
+// })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+    Person.findById(request.params.id).then(res => {
+        response.json(res)
+    })
+  })
+// app.delete('/api/persons/:id', (request, response) => {
+//   Person.findById(request.params.id).then(res => {
+//       response.json(res)
+//   })
+// })
 
-    if(person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-        console.log(response.status(404).end())
-    }
-})
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
-})
-
-const idGenerator = () => {
-    const randomId = Math.floor(Math.random() * 1000) 
-    const minId = persons.length + 1 
+// const idGenerator = () => {
+//     const randomId = Math.floor(Math.random() * 1000) 
+//     const minId = persons.length + 1 
     
-    //If randomId is less than minId, then minId will be set as an id
-    if(randomId >= minId ){
-        //check and prevent duplicatation of id
-        if(persons.find(num => num.id === randomId)){
-            return minId
-        } else {
-        return randomId
-    } 
-    }   else {
-        return minId
-    }
-}
+//     //If randomId is less than minId, then minId will be set as an id
+//     if(randomId >= minId ){
+//         //check and prevent duplicatation of id
+//         if(persons.find(num => num.id === randomId)){
+//             return minId
+//         } else {
+//         return randomId
+//     } 
+//     }   else {
+//         return minId
+//     }
+// }
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
-    const personFinder = persons.find(pers => pers.name === body.name)
-
-    if(!body.name){
-        return response.status(400).json({
-            error: 'Name is missing'
-        })
-    } else if (!body.number){
-        return response.status(400).json({
-            error: 'Number is missing'
-        })
-    } else if (personFinder){
-        return response.status(400).json({
-            error: `${body.name} already exists`
-        })
+    if(body.name === undefined){
+        return response.status(404).json({error: 'Content is missing'})
     }
 
-    const personObject = {
-        id: idGenerator(),
+    // if(!body.name){
+    //     return response.status(400).json({
+    //         error: 'Name is missing'
+    //     })
+    // } else if (!body.number){
+    //     return response.status(400).json({
+    //         error: 'Number is missing'
+    //     })
+    // // } 
+    // else if (personFinder){
+    //     return response.status(400).json({
+    //         error: `${body.name} already exists`
+    //     })
+    // }
+    console.log('body content is ')
+    console.log(body)
+    const contact = new Person({
         name: body.name,
         number: body.number
-    }
+  })
 
-    persons = persons.concat(personObject)
-
-    console.log(persons)
-    response.json(personObject)
+    contact.save().then(savedContact => {
+        console.log(savedContact)
+        response.json(savedContact)
+    })
 })
 
 
+
 app.get('/info', (request, response) => {
-    const amount = persons.length
+    const amount = Person.length
     const time = new Date()
     console.log(time)
     response.send(`<p>Phonebook has info for ${amount} people</p>
@@ -125,7 +135,7 @@ app.get('/info', (request, response) => {
 })
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
 })
