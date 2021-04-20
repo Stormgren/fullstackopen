ac2ffb8816da3e7f2192f396b60bf3ed0594dd16
Stormgren <sbnv93@gmail.com>
2021-04-20 04:08:22 +0200

    Finished parts 3.15 and 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -16,30 +16,15 @@ morgan.token('body', (request, response) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-// let persons = [
-//     {
-//         id: 1,
-//         name: "Arto Hellas",
-//         number: "040-123456"
-//     },
-//     {
-//         id: 2,
-//         name: "Ada Lovelace",
-//         number: "39-44-5323523"
-//     },
-//     {
-//         id: 3,
-//         name: "Dan Abramov",
-//         number: "12-43-234345"
-//     },
-//     {
-//         id: 4,
-//         name: "Marry Poppendick",
-//         number: "39-23-6423122"
-//     }
-    
-// ]
+  if( error.name === 'CastError') {
+    return response.status(400).send({error: 'malformated id'})
+  }
+
+  next(error)
+}
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(data => {
@@ -47,81 +32,20 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-// app.get('/api/persons/:id', (request, response) => {
-//     const id = String(request.params.id)
-//     const person = persons.find(person => person.id === id)
-
-//     if(person) {
-//         response.json(person)
-//     } else {
-//         response.status(404).end()
-//         console.log(response.status(404).end())
-//     }
-// })
 
 app.get('/api/persons/:id', (request, response) => {
     Person.findById(request.params.id).then(res => {
         response.json(res)
     })
   })
-// app.delete('/api/persons/:id', (request, response) => {
-//   Person.findById(request.params.id).then(res => {
-//       response.json(res)
-//   })
-// })
 
 
-// const idGenerator = () => {
-//     const randomId = Math.floor(Math.random() * 1000) 
-//     const minId = persons.length + 1 
-    
-//     //If randomId is less than minId, then minId will be set as an id
-//     if(randomId >= minId ){
-//         //check and prevent duplicatation of id
-//         if(persons.find(num => num.id === randomId)){
-//             return minId
-//         } else {
-//         return randomId
-//     } 
-//     }   else {
-//         return minId
-//     }
-// }
-
-// app.post('/api/persons', (request, response) => {
-//     const body = request.body
-
-//     if(body.name === undefined){
-//         return response.status(404).json({error: 'Content is missing'})
-//     }
-
-    // if(!body.name){
-    //     return response.status(400).json({
-    //         error: 'Name is missing'
-    //     })
-    // } else if (!body.number){
-    //     return response.status(400).json({
-    //         error: 'Number is missing'
-    //     })
-    // // } 
-    // else if (personFinder){
-    //     return response.status(400).json({
-    //         error: `${body.name} already exists`
-    //     })
-    // }
-//     console.log('body content is ')
-//     console.log(body)
-
-//     const contact = new Person({
-//         name: body.name,
-//         number: body.number
-//   })
-
-//     contact.save().then(savedContact => {
-//         console.log(savedContact)
-//         response.json(savedContact)
-//     })
-// })
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id).then(result => {
+      response.status(204).end()
+  })
+  .catch(error => next(error))
+})
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -150,6 +74,7 @@ app.get('/info', (request, response) => {
     
 })
 
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
